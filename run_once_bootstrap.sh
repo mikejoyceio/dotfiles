5502bf037fdcc6addec86b5412e275e2fdd8bbca
Mike Joyce <contact@mikejoyce.me>
2021-07-11 06:44:02 +0700

    Add run_once_bootstrap.sh

---
+++ run_once_bootstrap.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+# Setup script for macOS
+
+# Homebrew packages
+PACKAGES=(
+  bash-completion
+  bash-git-prompt
+  docker
+  gatsby-cli
+  mas
+  node
+  nvm
+  spoof-mac
+  tldr
+  yarn
+)
+
+# Homebrew casks
+CASKS=(
+  1password
+  1password-cli
+  atom
+  gifox
+  gimp
+  google-chrome
+  keybase
+  lepton
+  pixelsnap
+  sip
+  sizzy
+  sketch
+  skitch
+  skype
+  slack
+)
+
+# Mac App Store apps
+MAS=(
+  # Bear
+  1091189122
+  # LINE
+  539883307
+  # Numbers
+  409203825
+  # Pages
+  409201541
+  # Pocket
+  568494494
+  # Spark
+  1176895641
+)
+
+echo "Starting macOS bootstrap"
+
+# Check if Install Xcode CLI is present, install if not
+if test ! $(which xcode-select); then
+ echo "Install Xcode CLI..."
+ xcode-select --install
+fi
+
+# Check if Homebrew is present, install if not
+if test ! $(which brew); then
+ echo "Installing Homebrew..."
+ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew
+brew update
+
+echo "Install Homebrew packages..."
+brew install ${PACKAGES[@]}
+
+echo "Install Homebrew casks..."
+brew install --cask ${CASKS[@]}
+
+echo "Install Mac App Store apps..."
+mas install ${MAS[@]}
+
+# Install Shopify Theme Kit
+brew tap shopify/shopify
+brew install themekit
+
+echo "Clean up Homebrew"
+brew cleanup
+
+echo "macOS bootstrap complete!"
