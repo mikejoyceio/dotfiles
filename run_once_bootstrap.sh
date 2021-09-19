730b476380eef5c02553268b3240dfa770a8e548
Mike Joyce <contact@mikejoyce.me>
2021-09-19 18:51:49 +0700

    Fix postgresql package installation
    - Replace postgresql@12 with postgresql
    - Create user

--- run_once_bootstrap.sh
+++ run_once_bootstrap.sh
@@ -14,7 +14,7 @@ PACKAGES=(
   netlify-cli
   node
   nvm
-  postgresql@12
+  postgresql
   spoof-mac
   tldr
   yarn
@@ -99,5 +99,8 @@ brew cleanup
 # Install RVM
 gpg --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
+
+# Create PostgreSQL User
+/usr/local/opt/postgres/bin/createuser -s postgres
 
 echo "macOS bootstrap complete!"
