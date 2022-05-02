95d7a012c9ad68f50a1d2ee4e6822dd23ab88d3b
Mike Joyce <contact@mikejoyce.me>
2022-05-02 16:08:17 +0700

    Add dot_Brewfile

--- run_once_bootstrap.sh
+++ run_once_bootstrap.sh
@@ -1,71 +1,6 @@
 #!/usr/bin/env bash
 
-# Setup script for macOS
-
-# Homebrew packages
-PACKAGES=(
-  angular-cli
-  awscli
-  bash-completion
-  bash-git-prompt
-  contentful-cli
-  docker
-  gatsby-cli
-  gnupg
-  graphviz
-  hugo
-  localstack
-  mas
-  netlify-cli
-  node
-  nvm
-  postgresql
-  redis
-  spoof-mac
-  tldr
-  vue-cli
-  yarn
-)
-
-# Homebrew casks
-CASKS=(
-  1password-cli
-  atom
-  docker
-  figma
-  firefox
-  gifox
-  gimp
-  keybase
-  lepton
-  notion
-  pixelsnap
-  postman
-  sip
-  sizzy
-  sketch
-  skitch
-  skype
-  slack
-)
-
-# Mac App Store apps
-MAS=(
-  # 1password
-  1333542190
-  # LINE
-  539883307
-  # Numbers
-  409203825
-  # Pages
-  409201541
-  # Pocket
-  568494494
-  # Spark
-  1176895641
-)
-
-echo "Starting macOS bootstrap"
+echo "Start macOS bootstrap"
 
 # Install Xcode CLI
 if test ! $(which xcode-select); then
@@ -75,42 +10,26 @@ fi
 
 # Install Vim Plug
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
- echo "Installing Vim Plug..."
+ echo "Install Vim Plug..."
  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # Install Homebrew
 if test ! $(which brew); then
- echo "Installing Homebrew..."
+ echo "Install Homebrew..."
  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Update Homebrew
+# Install Homebrew packages
 brew update
-
-echo "Install Homebrew packages..."
-brew install ${PACKAGES[@]}
-
-echo "Install Homebrew casks..."
-brew install --cask ${CASKS[@]}
-
-echo "Install Mac App Store apps..."
-mas install ${MAS[@]}
-
-# Install Shopify Theme Kit
-brew tap shopify/shopify
-brew install themekit
-
-echo "Clean up Homebrew"
+brew bundle --global
 brew cleanup
-
-# Install RVM
-gpg --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | bash -s stable
 
 # Create PostgreSQL User
 brew services restart postgresql
 /usr/local/opt/postgres/bin/createuser -s postgres
 
-echo "macOS bootstrap complete!"
+# Install RVM
+gpg --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
