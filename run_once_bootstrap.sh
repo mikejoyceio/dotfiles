2f16387e83340186a3b23c5b1cdd168195272c6f
mikejoyceio <contact@mikejoyce.me>
2022-11-03 12:42:53 +0700

    Update Homebrew installer

--- run_once_bootstrap.sh
+++ run_once_bootstrap.sh
@@ -18,7 +18,7 @@ fi
 # Install Homebrew
 if test ! $(which brew); then
  echo "Install Homebrew..."
- ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Install Homebrew packages
